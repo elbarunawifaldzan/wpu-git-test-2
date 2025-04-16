455ca87d096bb60190bdab3d6ecdd116c9007519
Elbaruna Wifaldzan <elbarunawifaldzan@gmail.com>
2025-04-16 11:02:21 +0700

    menambahkan paragrapt

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello Word</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolore impedit earum consectetur aut inventore quam dicta perspiciatis laudantium. Quia adipisci modi voluptatibus nobis ipsam voluptatem amet sequi ex consectetur!</p>
 </body>
 
 </html>
